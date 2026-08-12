a68273c673c533cb7ab1b563660213801155224e
SHS234 <2623617234@qq.com>
2026-08-12 12:05:47 +0800

    fix: 改为 /journal 子路径

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/journal",
   images: {
     unoptimized: true,  // GitHub Pages 不支持 Next.js 图片优化
   },
